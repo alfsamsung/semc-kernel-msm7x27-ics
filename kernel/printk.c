240a4c685ecedde5c47c557848a6b462705136b9
alfsamsung <alfsamsung@hotmail.com>
2012-10-16 19:52:54 +0200

    kernel: printk: print limited chars in release_console_sem
    There could be lot of log accumulated while someone holds the
    console_sem. Printing all of them with irq disabled causes interrupt
    starvation. Print limited number of characters and after that enable
    interrupts and require the spin lock to print the next set of
    characters.
    
    Limiting the size to 128 characters was chosen because it takes around
    11ms to send them at baud rate of 115200. Disabling interrupts for
    11ms to chug through the accumulated log is reasonable.
    
    Change-Id: I595faf0d8d36f3107672aefdbf0faf862635d1ba
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- kernel/printk.c
+++ kernel/printk.c
@@ -132,6 +132,8 @@ EXPORT_SYMBOL(console_set_on_cmdline);
 /* Flag: console code may call schedule() */
 static int console_may_schedule;
 
+#define MAX_CHARS_PER_RELEASE_LOOP 128
+
 #ifdef CONFIG_PRINTK
 
 static char __log_buf[__LOG_BUF_LEN];
@@ -1049,8 +1051,9 @@ void release_console_sem(void)
 		if (con_start == log_end)
 			break;			/* Nothing to print */
 		_con_start = con_start;
-		_log_end = log_end;
-		con_start = log_end;		/* Flush */
+		_log_end = (con_start + MAX_CHARS_PER_RELEASE_LOOP < log_end) ?
+			con_start + MAX_CHARS_PER_RELEASE_LOOP : log_end;
+		con_start = _log_end;           /* Flush */
 		spin_unlock(&logbuf_lock);
 		stop_critical_timings();	/* don't trace print latency */
 		call_console_drivers(_con_start, _log_end);
