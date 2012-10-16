f91ce8b61e1517432a55703ff32ad85bf777a761
alfsamsung <alfsamsung@hotmail.com>
2012-10-16 19:45:37 +0200

    Revert part 2

--- drivers/char/vt.c
+++ drivers/char/vt.c
@@ -2138,7 +2138,6 @@ static int do_con_write(struct tty_struct *tty, const unsigned char *buf, int co
 	if (!vc_cons_allocated(currcons)) {
 	    /* could this happen? */
 	    printk_once("con_write: tty %d not allocated\n", currcons+1);
-	    }
 	    release_console_sem();
 	    return 0;
 	}
